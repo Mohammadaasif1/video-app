0a23c150ea062599390f0e25fd92e5607ce5215f
mohammadaasif1 <asif00077786@gmail.com>
2024-06-19 10:24:57 +0530

    mongo db connected with server

--- src/index.js
+++ src/index.js
@@ -1,2 +1,42 @@
+// require ('dotenv').config({path: './env'})
+import dotenv from "dotenv";
+import express from "express";
+import connectDb from "./db/index.js";
 
+dotenv.config({path: './env'})
+
+const app = express();
+
+
+
+connectDb();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ;( async()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//         app.on("error",()=>{
+//             console.log("ERROR:" ,error);
+//             throw error;
+//         })
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`app is listing on Port:${process.env.PORT}`);
+//         })
+//     } catch (error) {
+//         console.error("error:", error);
+//         throw error
+//     }
+// }) ()
 
